4111e08ff2eb0687b9cca1a9efa093b265cd7911
Thomas Habets <thomas@habets.pp.se>
2009-06-04 23:04:54 +0200

    openpty() implemented with _getpty() (IRIX)

--- openpty_getpty.c
+++ openpty_getpty.c
@@ -1,8 +1,8 @@
-/* ind/pty_socketpair.c - fake openpty() using socketpair()
+/* ind/pty_getpty.c
  *
  * (BSD license without advertising clause below)
  *
- * Copyright (c) 2005-2008 Thomas Habets. All rights reserved.
+ * Copyright (c) 2009 Thomas Habets. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,51 +31,81 @@
 #endif
 
 static const int ISO_C_forbids_an_empty_source_file = 1;
+
 #ifdef HAVE_OPENPTY
-/* system has real openpty() */
+/* */
 #elif defined (__SVR4) && defined (__sun)
-/* We have one locally for Solaris */
+/* */
 #elif defined (HAVE__GETPTY)
-/* We have one that is based on _getpty() */
-#else
-/* don't have real openpty(), and have no fix. Fake using socketpair() */
 #include <utmp.h>
 #include <unistd.h>
+#include <strings.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stropts.h>
+#include <errno.h>
+#include <stropts.h>
+#include <termios.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <strings.h>
-#include <errno.h>
+#include <sys/stat.h>
 
 struct winsize;
 struct termios;
 
+int do_close(int fd);
+
 /**
- *
+ * taken from pts(7D), and added error handling
  */
 int
-openpty(int  *amaster,  int  *aslave,  char  *name, struct termios
-	*termp, struct winsize * winp)
+openpty(int  *amaster, int *aslave, char *name,
+	struct termios *termp, struct winsize *winp)
 {
-  int fd[2];
+  int fds = -1;
+  int fdm = -1;
   int saved_errno = 0;
-
-  if (socketpair(AF_UNIX, SOCK_STREAM, 0, fd)) {
-    return -1;
+  char *line;
+  
+  line = _getpty(&fdm, O_RDWR|O_NDELAY, 0600, 0);
+  if (!line) {
+    fprintf(stderr, "_getpty() failed\n");
+    goto errout;
   }
-  *amaster = fd[0];
-  *aslave = fd[1];
+  if (0 > (fds = open(line, O_RDWR))) {
+    goto errout;
+  }
+
+
+  *amaster = fdm;
+  *aslave = fds;
+
   if (name) {
-    strcpy(name, "/dev/FIXME");
+    strcpy(name, ptsname(*amaster));
+  }
+  printf("%d %d\n", *amaster, *aslave);
+  if (termp) {
+    if (0 > tcsetattr(*aslave, TCSADRAIN, termp)) {
+      fprintf(stderr, "lala\n");
+      goto errout;
+    } 
+  }
+  if (winp) {
+    if (0 > ioctl(*aslave, TIOCSWINSZ, winp)) {
+      fprintf(stderr, "lolo\n");
+      goto errout;
+    }
   }
   return 0;
 
  errout:
   saved_errno = errno;
-  if (0 < fd[0]) {
-    close(fd[0]);
+  if (0 < fds) {
+    do_close(fds);
   }
-  if (0 < fd[1]) {
-    close(fd[1]);
+  if (0 < fdm) {
+    do_close(fdm);
   }
   if (name) {
     *name = 0;
@@ -85,38 +115,10 @@ openpty(int  *amaster,  int  *aslave,  char  *name, struct termios
 }
 
 /**
- *
- */
-int
-login_tty(int fd)
-{
-#ifdef NOT_A_TTY
-  if (0 > setsid()) {
-    return -1;
-  }
-  if (ioctl(fd, TIOCSCTTY, 1) < 0) {
-    return -1;
-  }
-#endif
-  if (0 > dup2(fd, 0)) {
-    return -1;
-  }
-  if (0 > dup2(fd, 1)) {
-    return -1;
-  }
-  if (0 > dup2(fd, 2)) {
-    return -1;
-  }
-  close(fd);
-  return 0;
-}
-
-/**
  * Local Variables:
  * mode: c
  * c-basic-offset: 2
  * fill-column: 79
  * End:
  */
-
 #endif
